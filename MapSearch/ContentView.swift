ef34b4c2cf902fc3eb2c30049cf8c8ad919d94bd
CeciliaChen <cecilia@pdotf.com>
2023-08-08 03:12:45 -0400

    Initial Commit

---
+++ MapSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapSearch
+//
+//  Created by Cecilia Chen on 8/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapSearch/MapSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSearchApp.swift
+//  MapSearch
+//
+//  Created by Cecilia Chen on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
